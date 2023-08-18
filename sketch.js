e3035f525550bc5c667ba614e09d7527f1350ec8
Ali Torbati <ali.torbati@gmail.com>
2023-08-18 13:25:24 -0700

    optimizations

--- sketch.js
+++ sketch.js
@@ -8,7 +8,6 @@ let angle = 0;
 let typeImg;
 let unitSize = 300;
 let canvas;
-let activityDetected = false;
 
 function preload() {
   typeImg = loadImage("mbh_2048.png");
@@ -18,47 +17,31 @@ function setup() {
   canvas = createCanvas(windowWidth, windowHeight, WEBGL);
   canvas.position(0, 0);
   canvas.style("z-index", "-1");
+  ambientLight(100);
+  directionalLight(255, 255, 255, 0, 0, -1);
 }
 
 function draw() {
+  if (mouseX === 0 && mouseY === 0) return false;
+
   background(0);
-  noStroke();
-  ambientLight(100);
-  directionalLight(255, 255, 255, 0, 0, -1);
+  unitSize = map(mouseX, 0, width, 200, width);
+  angle += 0.01;
 
-  if (activityDetected == false) {
-    push();
-    translate(0, 0, -width);
-    texture(typeImg);
-    box(width/2);
-    pop();
-  }
-
-  if (activityDetected == true) {
-    for (let x = -width / 2; x <= width / 2; x += unitSize) {
-      for (let y = -height / 2; y <= height / 2; y += unitSize) {
-        push();
-        translate(x, y, 0);
-        rotateX(angle);
-        rotateY(angle * 1.3);
-        rotateZ(angle * 0.7);
-        texture(typeImg);
-        box(unitSize);
-        pop();
-      }
+  for (let x = -width / 2; x <= width / 2; x += unitSize) {
+    for (let y = -height / 2; y <= height / 2; y += unitSize) {
+      push();
+      translate(x, y, 0);
+      rotateX(angle);
+      rotateY(angle * 1.3);
+      rotateZ(angle * 0.7);
+      texture(typeImg);
+      box(unitSize);
+      pop();
     }
-
-    unitSize = map(mouseX, 0, width, 200, width);
-    angle += 0.01;
   }
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
-}
-
-function mouseMoved() {
-  if (activityDetected == false) {
-    activityDetected = true;
-  }
 }
